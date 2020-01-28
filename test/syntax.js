f662499a538a0d32c526e276bc45b57a42e4c085
BenjaminJ <kamikillerto@gmail.com>
2020-01-28 18:38:07 +0100

    build: Update highlight.js

--- test/syntax.js
+++ test/syntax.js
@@ -7,13 +7,12 @@ describe('Syntax highlight', function() {
     beforeEach(function() {
       this.load('``` html\n<div>hello</div>\n```');
     });
-    it('should work', function() {});
     it('intact text', function() {
       expect(this.$('pre').text().trim()).eq("<div>hello</div>");
     });
     it('highlight', function() {
       expect(this.$).have.selector('pre .hljs-tag');
-      expect(this.$).have.selector('pre .hljs-title');
+      expect(this.$).have.selector('pre .hljs-name');
     });
     it('set correct pre class', function() {
       expect(this.$).have.selector('pre.sg-lang-html');
@@ -23,11 +22,10 @@ describe('Syntax highlight', function() {
     beforeEach(function() {
       this.load('### Buttons\n\n    @example\n    a.button.primary Primary button\n    a.button.success Success button');
     });
-    it('should work', function() {});
     it('should highlight', function() {
       expect(this.$('.sg-code .hljs-tag').length).gte(4);
-      expect(this.$('.sg-code .hljs-value').length).gte(2);
-      expect(this.$('.sg-code .hljs-attribute').length).gte(2);
+      expect(this.$('.sg-code .hljs-string').length).gte(2);
+      expect(this.$('.sg-code .hljs-attr').length).gte(2);
       expect(this.$('.sg-code').html()).match(/&lt;/);
     });
   });
