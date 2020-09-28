29a1a255933f00ea06274e9789727d2792419cce
BenjaminJ <kamikillerto@gmail.com>
2020-09-28 09:36:42 +0200

    fix: Remove trailing slash only for none SVG tags

--- test/pretty_print.js
+++ test/pretty_print.js
@@ -1,3 +1,4 @@
+const { beautifyHTML } = require('../lib/utils')
 const setupTestEnv = require('./setup')
 
 describe('Pretty Print', function() {
@@ -9,6 +10,7 @@ describe('Pretty Print', function() {
         head: ''
       })
     })
+
     it('indent <head>', function() {
       expect(this.html).match(/\n {2}<head/)
     })
@@ -22,17 +24,34 @@ describe('Pretty Print', function() {
       expect(this.html).match(/\n {10}<div class="sg-canvas/)
     })
   })
+
   describe('custom indentSize', function() {
     beforeEach(function() {
       this.load("### Hello\n\n    @example\n    div", {
         tabWidth: 4
       })
     })
+
     it('indent .sg-section-hello', function() {
       expect(this.html).match(/<section class="sg-block sg-section-hello/)
     })
     it('indent .sg-canvas', function() {
       expect(this.html).match(/\n {8}<div class="sg-canvas/)
     })
+  })
+})
+
+describe('beautifyHTML | Remove trailing slash self closing tags', function(){
+  it('Remove trailing slash for img tag', function() {
+    let html = beautifyHTML('<img src="foo.bar" alt="Foo image"/>')
+    expect(html).match(/<img src="foo.bar" alt="Foo image" >/)
+  })
+  it("Keep trailing slash for SVG's children tags", function() {
+    let html = beautifyHTML(`
+    <svg>
+      <use xlink:href="../icons.svg#checkmark" href="../icons.svg#checkmark"/>
+    </svg>
+    `)
+    expect(html).match(/<use xlink:href="..\/icons.svg#checkmark" href="..\/icons.svg#checkmark" \/>/)
   })
 })
